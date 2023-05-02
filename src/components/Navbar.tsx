f177f7fccc51a31e21e90b2c635df8f4a831e9f1
Eliot Rhys <32644073+eliotrhys@users.noreply.github.com>
2023-05-02 10:15:35 +0100

    somethign

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,12 +5,12 @@ const Navbar = () => {
             <div>🌱 The Lifereel Company</div>
 
             <div className="flex items-center">
-                <div className="mr-2">Home</div>
-                <div className="mr-2">Features</div>
-                <div className="mr-2">Examples</div>
-                <div className="mr-2">About</div>
-                <div className="mr-2">FAQ</div>
-                <div className="mr-2">Pricing</div>
+                <div className="mr-4">Home</div>
+                <div className="mr-4">Features</div>
+                <div className="mr-4">Examples</div>
+                <div className="mr-4">About</div>
+                <div className="mr-4">FAQ</div>
+                <div className="mr-4">Pricing</div>
                 <div>Contact</div>
             </div>
            
