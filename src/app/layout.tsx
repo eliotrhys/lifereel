9c655d833f80aa995c4d9cf74e5528da180f0ab5
Eliot Rhys <32644073+eliotrhys@users.noreply.github.com>
2025-02-05 11:10:46 +0000

    dangerously set html

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,6 @@ import favicon from "./favicon.png";
 import Script from 'next/script';
 import { useEffect } from 'react';
 
-const META_PIXEL_ID = "3828535074040605";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -36,45 +35,7 @@ export default function RootLayout({ children } : { children: React.ReactNode })
         <meta property="og:image" content="/images/og_lifereel.jpg" />
         <meta name="keywords" content="Lifereel, lifereel, lifereel company, the lifereel company, The Lifereel Company, the Lifereel Company, video memoirs, memoirs, autobiography, autobiografilm, family history, genealogy" />
 
-        {/* Load Meta Pixel Script */}
-        <Script
-          id="meta-pixel"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-            if (!window.fbq) {
-                !function(f,b,e,v,n,t,s)
-                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                n.queue=[];t=b.createElement(e);t.async=!0;
-                t.src=v;s=b.getElementsByTagName(e)[0];
-                s.parentNode.insertBefore(t,s)}(window, document,'script',
-                'https://connect.facebook.net/en_US/fbevents.js');
-                fbq('init', '${META_PIXEL_ID}');
-                fbq('track', 'PageView');
-
-
-                // Prevent duplicate PageView events from Calendly
-                const originalFbq = fbq;
-                fbq = function(event, ...args) {
-                  if (event === "PageView" && window.fbqPageViewTracked) {
-                    console.log("Duplicate PageView blocked.");
-                    return;
-                  }
-                  if (event === "PageView") {
-                    window.fbqPageViewTracked = true;
-                  }
-                  originalFbq(event, ...args);
-                };
-            }
-            `,
-          }}
-        />
-
-        <noscript>
-          <img height="1" width="1" style={{display: "none"}} src="https://www.facebook.com/tr?id=3828535074040605&ev=PageView&noscript=1"/>
-        </noscript>
+        
       </head>
      
       <body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,8 @@ import Examples from '@/components/Examples'
 import LifereelAcademy from '@/components/LifereelAcademy'
 import { useState } from 'react'
 import SideMenu from '@/components/SideMenu'
+import Head from 'next/head';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -24,9 +26,37 @@ export default function Home() {
   const handleMenuToggle = () => {
     setIsMenuOpen((prevIsMenuOpen) => !prevIsMenuOpen);
   };
+
+  const META_PIXEL_ID = "3828535074040605";
   
   return (
     <main className="">
+      <Head>
+        {/* Load Meta Pixel Script */}
+        <script
+          id="meta-pixel"
+          dangerouslySetInnerHTML={{
+            __html: `
+            if (!window.fbq) {
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '${META_PIXEL_ID}');
+                fbq('track', 'PageView');
+            }
+            `,
+          }}
+        />
+
+        <noscript>
+          <img height="1" width="1" style={{display: "none"}} src="https://www.facebook.com/tr?id=3828535074040605&ev=PageView&noscript=1"/>
+        </noscript>
+      </Head>
       <SideMenu isOpen={isMenuOpen} onMenuToggle={handleMenuToggle} />
       <Navbar onMenuToggle={handleMenuToggle} />
       <Jumbotron />
